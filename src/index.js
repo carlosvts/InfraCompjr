32e8378f35fdfd3625526525c0eb30a9b5945cce
carlosvts <carlosvtsdev@gmail.com>
2026-04-20 21:51:14 -0300

    feat: adiciona rota /

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok" });
 });
 
+app.get("/", (req, res) => {
+  res.send("API rodando, Comp! 🚀");
+});
+
 // porta para o Render
 const PORT = process.env.PORT || 3000;
 
